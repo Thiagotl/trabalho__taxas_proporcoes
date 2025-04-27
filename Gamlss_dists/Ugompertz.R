f6befc0aea2fb5e6beb5d2f3916939af4472f1fd
Leonardo Migliorini <leonardopriormigliorini@gmail.com>
2025-04-26 23:57:53 -0300

    Identando os scripts

--- Gamlss_dists/Ugompertz.R
+++ Gamlss_dists/Ugompertz.R
@@ -9,64 +9,55 @@ dldd_expr <- D(loglikUGo, "sigma")
 #### DEFINIÇÃO DA FAMÍLIA UGo PARA GAMLSS ----
 
 UGo <- function(mu.link = "logit", sigma.link = "log") {
-  mstats <- checklink("mu.link", "UGo", substitute(mu.link),
-                      c("logit", "probit", "cloglog", "cauchit", "log", "own"))
-  dstats <- checklink("sigma.link", "UGo", substitute(sigma.link),
-                      c("inverse", "log", "identity", "own"))
-  
+  mstats <- checklink(
+    "mu.link", "UGo", substitute(mu.link),
+    c("logit", "probit", "cloglog", "cauchit", "log", "own")
+  )
+  dstats <- checklink(
+    "sigma.link", "UGo", substitute(sigma.link),
+    c("inverse", "log", "identity", "own")
+  )
+
   structure(list(
     family = c("UGo", "Unit-Gompertz"),
     parameters = list(mu = TRUE, sigma = TRUE),
     nopar = 2,
     type = "Continuous",
     control = list(trace = FALSE),
-    
     mu.link = as.character(substitute(mu.link)),
     sigma.link = as.character(substitute(sigma.link)),
-    
     mu.linkfun = mstats$linkfun,
     sigma.linkfun = dstats$linkfun,
-    
     mu.linkinv = mstats$linkinv,
     sigma.linkinv = dstats$linkinv,
-    
     mu.dr = mstats$mu.eta,
     sigma.dr = dstats$mu.eta,
-    
     dldm = function(y, mu, sigma) eval(dldm_expr),
-    
     d2ldm2 = function(y, mu, sigma) {
       dldm <- eval(dldm_expr)
       d2 <- -dldm^2
       ifelse(d2 < -1e-15, d2, -1e-15)
     },
-    
     dldd = function(y, mu, sigma) eval(dldd_expr),
-    
     d2ldd2 = function(y, mu, sigma) {
       dldd <- eval(dldd_expr)
       d2 <- -dldd^2
       ifelse(d2 < -1e-15, d2, -1e-15)
     },
-    
     d2ldmdd = function(y, mu, sigma) {
       dldm <- eval(dldm_expr)
       dldd <- eval(dldd_expr)
       d2 <- -dldm * dldd
       ifelse(is.na(d2), 0, d2)
     },
-    
     G.dev.incr = function(y, mu, sigma, ...) {
       -2 * dUGo(y = y, mu = mu, sigma = sigma, log = TRUE)
     },
-    
     rqres = expression(
       rqres(pfun = "pUGo", type = "Continuous", y = y, mu = mu, sigma = sigma)
     ),
-    
     mu.initial = expression(mu <- rep(0.5, length(y))),
     sigma.initial = expression(sigma <- rep(1, length(y))),
-    
     mu.valid = function(mu) all(mu > 0 & mu < 1),
     sigma.valid = function(sigma) all(sigma > 0),
     y.valid = function(y) all(y > 0 & y < 1)
@@ -78,9 +69,9 @@ dUGo <- function(y, mu, sigma, log = FALSE) {
   if (any(y <= 0 | y >= 1)) {
     return(if (log) rep(-Inf, length(y)) else rep(0, length(y)))
   }
-  
+
   log_fx <- log(mu) + log(sigma) - (sigma + 1) * log(y) - mu * (y^(-sigma) - 1)
-  
+
   if (log) {
     return(log_fx)
   } else {
@@ -88,9 +79,8 @@ dUGo <- function(y, mu, sigma, log = FALSE) {
   }
 }
 
-pUGo<-function(q, mu, sigma, lower.tail = TRUE, log.p = FALSE)
-{
-  cdf<-exp(-mu*(q^(-sigma)-1))
+pUGo <- function(q, mu, sigma, lower.tail = TRUE, log.p = FALSE) {
+  cdf <- exp(-mu * (q^(-sigma) - 1))
 
   return(cdf)
 }
--- Gamlss_dists/Utriangular.R
+++ Gamlss_dists/Utriangular.R
@@ -1,35 +1,40 @@
-TRI<-function (mu.link = "logit")
-{
-  mstats <- checklink("mu.link", "TRI", substitute(mu.link),
-                      c("logit","probit","cloglog","cauchit","log","own","identity"))
-  structure(list(family = c("TRI"),
-                 parameters = list(mu = TRUE),
-                 nopar = 1,
-                 type = "Continuous",
-                 mu.link = as.character(substitute(mu.link)),
-                 mu.linkfun = mstats$linkfun,
-                 mu.linkinv = mstats$linkinv,
-                 mu.dr = mstats$mu.eta,
-                 dldm = function(y, mu) {
-                   ifelse(y < mu, -1 / mu, 1 / (1 - mu))
-                 },
-                 d2ldm2 = function(y,mu) {
-                   ifelse(y < mu, -1/mu^2, -1/(1-mu)^2)
-                 },
-                 G.dev.incr = function(y, mu, ...) -2 * dTRI(y=y,mu=mu,log=TRUE),
-                 rqres = expression(
-                   rqres(pfun="pTRI",type ="Continuous",y=y,mu=mu)
-                 ),
-                 mu.initial = expression(mu <- rep(mean(y),length(y))),
-                 mu.valid = function(mu) all(mu > 0 & mu < 1),
-                 y.valid = function(y) all(y > 0 & y < 1)),
-  class = c("gamlss.family", "family"))
+TRI <- function(mu.link = "logit") {
+  mstats <- checklink(
+    "mu.link", "TRI", substitute(mu.link),
+    c("logit", "probit", "cloglog", "cauchit", "log", "own", "identity")
+  )
+  structure(
+    list(
+      family = c("TRI"),
+      parameters = list(mu = TRUE),
+      nopar = 1,
+      type = "Continuous",
+      mu.link = as.character(substitute(mu.link)),
+      mu.linkfun = mstats$linkfun,
+      mu.linkinv = mstats$linkinv,
+      mu.dr = mstats$mu.eta,
+      dldm = function(y, mu) {
+        ifelse(y < mu, -1 / mu, 1 / (1 - mu))
+      },
+      d2ldm2 = function(y, mu) {
+        ifelse(y < mu, -1 / mu^2, -1 / (1 - mu)^2)
+      },
+      G.dev.incr = function(y, mu, ...) -2 * dTRI(y = y, mu = mu, log = TRUE),
+      rqres = expression(
+        rqres(pfun = "pTRI", type = "Continuous", y = y, mu = mu)
+      ),
+      mu.initial = expression(mu <- rep(mean(y), length(y))),
+      mu.valid = function(mu) all(mu > 0 & mu < 1),
+      y.valid = function(y) all(y > 0 & y < 1)
+    ),
+    class = c("gamlss.family", "family")
+  )
 }
 
-dTRI <- function(y, mu,log=FALSE) {
+dTRI <- function(y, mu, log = FALSE) {
   if (any(mu <= 0) | any(mu >= 1)) stop(paste("mu must be between 0 and 1", "\n", ""))
   if (any(y <= 0) | any(y >= 1)) stop(paste("y must be between 0 and 1", "\n", ""))
-  
+
   fy1 <- ifelse(
     y < 0 | y > 1,
     0,
@@ -39,32 +44,32 @@ dTRI <- function(y, mu,log=FALSE) {
       2 * (1 - y) / (1 - mu)
     )
   )
-  if(log==FALSE) fy<-fy1 else fy<-log(fy1)
+  if (log == FALSE) fy <- fy1 else fy <- log(fy1)
   return(fy)
 }
 
 # cumulative distribution function
-pTRI <- function(q, mu,lower.tail = TRUE, log.p = FALSE) {
+pTRI <- function(q, mu, lower.tail = TRUE, log.p = FALSE) {
   if (any(mu <= 0)) stop(paste("mu must be positive", "\n", ""))
   if (any(q <= 0) | any(q >= 1)) stop(paste("y must be between 0 and 1", "\n", ""))
-    Fx <- ifelse(
-      q < 0, 0,
+  Fx <- ifelse(
+    q < 0, 0,
+    ifelse(
+      q < mu,
+      (q^2) / mu,
       ifelse(
-        q < mu,
-        (q^2) / mu,
-        ifelse(
-          q > mu,
-          1 - ((1 - q)^2) / (1 - mu),
-          1
-        )
+        q > mu,
+        1 - ((1 - q)^2) / (1 - mu),
+        1
       )
     )
-    if(lower.tail==TRUE) cdf<-Fx else cdf<- 1-Fx
-    if(log.p==FALSE) cdf<- cdf else cdf<- log(cdf)
-    return(cdf)
+  )
+  if (lower.tail == TRUE) cdf <- Fx else cdf <- 1 - Fx
+  if (log.p == FALSE) cdf <- cdf else cdf <- log(cdf)
+  return(cdf)
 }
 
-#inversion method for randon generation
+# inversion method for randon generation
 rTRI <- function(n, mu) {
   u <- runif(n)
   x <- ifelse(
