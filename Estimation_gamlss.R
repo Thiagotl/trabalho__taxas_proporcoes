e9683ddee8b164c5f6f8f25aa97ef3afed86446d
Leonardo Migliorini <leonardopriormigliorini@gmail.com>
2025-04-27 06:26:02 -0300

    Melhorando o script

--- Estimation_gamlss.R
+++ Estimation_gamlss.R
@@ -1,4 +1,11 @@
-source("Gamlss_dists//Uchen.R", "Gamlss_dists//UGamma")
+# Loading gamlss distribution functions
+source("Gamlss_dists//Uchen.R")
+source("Gamlss_dists//UGamma.R")
+source("Gamlss_dists//Ugompertz.R")
+source("Gamlss_dists//UKumaraswamy.R")
+source("Gamlss_dists//ULindley.R")
+source("Gamlss_dists//UTriangular.R")
+source("Gamlss_dists//UWeibull.R")
 
 fitted_dist <- function(y, family) {
   # Define list of families and their arguments
@@ -9,25 +16,25 @@ fitted_dist <- function(y, family) {
     KW = list(fun = KW, args = list(mu.link = "identity", sigma.link = "identity")),
     UC = list(fun = UC, args = list(mu.link = "identity", sigma.link = "identity")),
     TRI = list(fun = TRI, args = list(mu.link = "identity")),
-    Ugo = list(fun = Ugo, args = list(mu.link = "identity", sigma.link = "identity")),
+    UGo = list(fun = UGo, args = list(mu.link = "identity", sigma.link = "identity")),
     BEo = list(fun = BEo, args = list(mu.link = "identity", sigma.link = "identity")),
     BE = list(fun = BE, args = list(mu.link = "identity", sigma.link = "identity")),
     SIMPLEX = list(fun = SIMPLEX, args = list(mu.link = "identity", sigma.link = "identity"))
   )
-
+  
   if (!family %in% names(families)) stop("Family not recognized.")
-
+  
   fam <- do.call(families[[family]]$fun, families[[family]]$args)
-
+  
   result <- suppressWarnings(try(
-    gamlss::gamlss(y ~ 1, sigma.formula = ~1, family = fam, trace = FALSE),
+    gamlss::gamlss(y ~ 1, sigma.formula = ~1, family = fam, trace = FALSE), 
     silent = TRUE
   ))
-
+  
   if (inherits(result, "try-error")) {
     warning("Model fitting failed.")
     return(NULL)
   } else {
-    return(summary(result))
+    return(result)
   }
 }
