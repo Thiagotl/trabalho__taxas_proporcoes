e05f6897694c65131d1cd103468ee3f710de9372
Leonardo Migliorini <leonardopriormigliorini@gmail.com>
2025-07-01 12:34:04 -0300

    Atualizando plots

--- Analise_descritiva.R
+++ Analise_descritiva.R
@@ -1,5 +1,8 @@
 # Loading packages and functions -----------------------------------------------
 library(ggplot2)
+library(dplyr)
+library(tidyr)
+options(OutDec=",") # opção para salvar plots com "," como separador decimal
 
 # Importing Data ---------------------------------------------------------------
 
@@ -43,86 +46,59 @@ dados <- readr::read_csv("data_sets/Sleep_Efficiency.csv") |>
     -c(1, 4, 5)
   )
 
+# Métricas variáveis
+summary(dados)
 
-summary(data_sleep[2])
-sqrt(var(data_sleep[2]))
-hist(data_sleep$Age, ylab = "Frequência", main = "Histograma das Idades", xlab = "Idade")
-# Observa-se que a maior parte dos dados são de adultos com média de 40 anos idade,
-# porém também contem crianças e idosos.
+# Histograma Variável resposta - eficiência do sono
+hist(dados$`Sleep efficiency`)
 
-data_sleep$sexo_bin <- ifelse(data_sleep$Gender == "Female", 1, 0)
-ggplot(data_sleep[3], aes(x = Gender)) +
-  geom_bar() +
-  labs(title = "Distribuição por Sexo", x = "Sexo", y = "Frequência") +
-  theme_minimal()
-# É a mesma quantidade, nem precisa de gráfico
+ggplot(dados, aes(x = `Sleep efficiency`)) +
+  geom_histogram(color = "black", fill = "white", bins = 10) +
+  theme_minimal(base_size = 14) +
+  theme(
+    panel.grid = element_blank(),         # Remove grid
+    panel.border = element_rect(color = "black", fill = NA, size = 1), # Adiciona borda
+    axis.line = element_blank()           # Remove linhas dos eixos para não sobrepor à borda
+  ) +
+  labs(
+    x = "Eficiência do Sono",
+    y = "Frequência",
+  )
 
-summary(data_sleep[6]) # duração do sono
-sqrt(var(data_sleep[6]))
-hist(data_sleep$`Sleep duration`, ylab = "Frequência", main = "Histograma da duração do Sono", xlab = "Duração (h)", breaks = 5)
-# A maioria dorme pelo menos 7 horas com uma média de 7.5 horas de sono, porém apenas 25% das pessoas
-# atinge as 8 horas de sono recomendadas
+ggsave("histograma_sono.pdf", width = 6, height = 4, units = "in")
 
-summary(data_sleep[7])
-sqrt(var(data_sleep[7]))
-hist(data_sleep$`Sleep efficiency`, ylab = "Frequência", main = "Histograma da eficiência do Sono", xlab = "Eficiência do Sono (%)", breaks = 5)
-# Todos tiveram pelo menos metade do sono aproveitado, em média tiverem 82% de eficiência no sono
+df <- dados |>
+  dplyr::select(
+    `Alcohol consumption`, `Smoking status`
+  ) |>
+  mutate(
+    `Alcohol consumption` = ifelse(`Alcohol consumption` == "1", "Sim", "Não"),
+    `Smoking status` = ifelse(`Smoking status` == "1", "Sim", "Não")
+  ) |>
+  rename(
+    `Consumiu Álcool` = `Alcohol consumption`,
+    Fumante = `Smoking status`
+  )
 
-summary(data_sleep[8])
-sqrt(var(data_sleep[8]))
-hist(data_sleep$`REM sleep percentage`, ylab = "Frequência", main = "Histograma do Pré-Sono (REM)", xlab = "Pré-Sono (%)", breaks = 3)
-# Tempo de transição entre acordado e sono leve, a maioria das pessoas teve 22% do sono dessa forma.
+# Continua igual ao código anterior, mas com dados padronizados
+df_long <- df %>%
+  pivot_longer(cols = everything(), names_to = "variavel", values_to = "resposta") %>%
+  group_by(variavel, resposta) %>%
+  summarise(n = n(), .groups = "drop") %>%
+  group_by(variavel) %>%
+  mutate(prop = n / sum(n))
 
-summary(data_sleep[9])
-sqrt(var(data_sleep[9]))
-hist(data_sleep$`Deep sleep percentage`, ylab = "Frequência", main = "Histograma do Sono Profundo", xlab = "Sono Profundo (%)")
-# Tempo de pesado, esse teve uma grande variação de 245! Acabou ficando divido ao meio, entre as pessoas
-# que conseguiram um sono profundo (a maioria) e as que não conseguiram.
-
-summary(data_sleep[10])
-sqrt(var(data_sleep[10]))
-hist(data_sleep$`Light sleep percentage`, ylab = "Frequência", main = "Histograma do Sono Leve", xlab = "Sono Leve (%)")
-# Praticamente o inverso do sono pesado.
-
-na11 <- na.omit(data_sleep[11])
-summary(na11)
-sqrt(var(na11))
-ggplot(na11, aes(x = Awakenings)) +
-  geom_bar() +
-  labs(title = "Gráfico de barras de quantas vezes acordou durante a noite", x = "Quantidade de vezes que acordou", y = "Frequência") +
-  theme_minimal()
-# Em média as pessoas acordaram 1 ou 2 vezes na noite.
-
-na12 <- na.omit(data_sleep[12])
-summary(na12)
-sqrt(var(na12))
-hist(data_sleep$`Caffeine consumption`, ylab = "Frequência", main = "Histograma do consumo de cafeína", xlab = "Consumo de Cafeína (g)")
-boxplot(data_sleep$`Caffeine consumption`, main = "Boxplot do Consumo de Cafeína", ylab = "Cafeína Ingerida (g)")
-# O consumo de cafeína foi em média 25 por pessoa, e apesar de que há diferença significativa
-# entre o consumo de cada um, a variância foi superestimada por um outlier que consumiu o DOBRO do segundo
-
-na13 <- na.omit(data_sleep[13])
-summary(na13)
-sqrt(var(na13))
-boxplot(data_sleep$`Alcohol consumption`, main = "Boxplot do Consumo de Álcool", ylab = "Álcool Ingerido (L)")
-# Assim como o outro vício, há uma discrepância grande nos dados, onde metade não ingeriu nada,
-# mas a média é de 1 por pessoas devido a outliers que consumiram muito alcool.
-
-smoking <- data_sleep$`Smoking status`
-
-data_sleep$cigas_bin <- ifelse(data_sleep$`Smoking status` == "Yes", 1, 0)
-ggplot(data_sleep[14], aes(x = smoking)) +
-  geom_bar() +
-  labs(title = "Gráfico sobre o consumo de tabaco", x = "Consumo de Tabaco", y = "Frequência") +
-  theme_minimal()
-# Aproximadamente 66% das pessoas da pesquisa não consumem tabaco.
-
-na14 <- na.omit(data_sleep[15])
-summary(na14)
-sqrt(var(na14))
-ggplot(na11, aes(x = Awakenings)) +
-  geom_bar() +
-  labs(title = "Gráfico de barras da frequência de atividade física", x = "Quantidade de vezes que se exercita na semana", y = "Frequência") +
-  theme_minimal()
-# Em média as pessoas acordaram se exercitam duas vezes por semana, tendo uma variação grande entre
-# os estudados, nesse caso um hábito saudável, mas assim como os vícios é difícil de balancear.
+ggplot(df_long, aes(x = variavel, y = prop, fill = resposta)) +
+  geom_bar(stat = "identity") +
+  scale_y_continuous(labels = scales::number_format(decimal.mark = ",")) +
+  labs(x = "", y = "Proporção", fill = "Resposta") +
+  scale_fill_manual(values = c("salmon", "skyblue")) +
+  theme_minimal(base_size = 14) +
+  theme(
+    panel.grid = element_blank(), # Remove grid
+    panel.border = element_rect(color = "black", fill = NA, size = 1), # Adiciona borda
+    axis.line = element_blank() # Remove linhas dos eixos para não sobrepor à borda
+  ) +
+  labs(
+    y = "Proporção",
+  )
